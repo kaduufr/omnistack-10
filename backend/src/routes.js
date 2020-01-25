36486a9e4ccc0228a50a2e16ae8b088ff7880d71
Carlos Eduardo <byedu1234@hotmail.com>
2020-01-25 10:48:58 -0300

    Finished frontend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,7 +6,6 @@ const routes = Router()
 
 routes.get('/devs', DevController.index)
 routes.post('/devs', DevController.store )
-routes.delete('/devs/:id', DevController.destroy)
 
 routes.get('/search', SearchController.index)
 
